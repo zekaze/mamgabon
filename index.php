e44b40fea6c317238cbe5ac8a3797c10078fd4e1
Alexandre Kaze <zekaze@users.noreply.github.com>
2018-04-16 23:31:37 +0100

    Bug fix Slide up animation

--- index.php
+++ index.php
@@ -19,8 +19,35 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-sm-9">body</div>
-        <div class="col-sm-3">
+        <div class="col-md-9">
+            <div class="articles">
+                <?php // cette boucle duplique la rangée de deux colonne;?>
+                <?php for ($i = 0; $i < 2; $i++): ?>
+                    <div class="row">
+                        <?php //cette boucle duplique la colonne "article";?>
+                        <?php for ($j = 0; $j < 2; $j++): ?>
+                            <div class="col-sm-6">
+                                <a href="#" class="article slideUp">
+                                    <div class="img-frame">
+                                        <img src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8aa38437c9b977558f5b430d68773742&auto=format&fit=crop&w=1352&q=80"
+                                             alt="image">
+                                    </div>
+                                    <h4 class="title">Lorem ipsum dolor sit amet,
+                                        consectetuer adipiscing elit, sed</h4>
+
+                                    <div class="abstract">Lorem ipsum dolor sit amet, consectetuer
+                                        adipiscing elit, sed diam nonummy nibh
+                                        euismod tincidunt ut laoreet dolore magna
+                                        aliquam erat volutpat.
+                                    </div>
+                                </a>
+                            </div>
+                        <?php endfor; ?>
+                    </div>
+                <?php endfor; ?>
+            </div>
+        </div>
+        <div class="col-md-3">
             <div class="sidebar">
                 <div class="calendar">calendar</div>
                 <a href="#" class="advert"><img src="#" alt="publicité"></a>
--- src/js/script.js
+++ src/js/script.js
@@ -84,3 +84,30 @@ $(window).scroll(function(){
         }
     });
 });
+
+/*
+var $animation_elements = jQuery('*[data-animate]'),
+    $window = jQuery(window);
+
+$window.on('scroll',check_if_in_view);
+function check_if_in_view() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+
+    $.each($animation_elements, function() {
+        var $element = jQuery(this);
+        var $animation = $element.attr('data-animate');
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+
+
+        //check to see if this current container is within viewport
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+            $element.addClass('animated data-animated '+$animation);
+        }
+    });
+}*/
