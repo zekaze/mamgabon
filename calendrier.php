41a8380c54daa1234e0a23532d19f7f5d7b81c8c
Alexandre Kaze <zekaze@users.noreply.github.com>
2018-06-02 17:49:33 +0100

    Updates: - Smooth scroll

--- calendrier.php
+++ calendrier.php
@@ -1,7 +1,6 @@
 <?php include "assets/header.php"; ?>
 
     <div class="container">
-        <span id="mois-juin"></span>
         <ul class="breadcrumbs left">
             <li><a href="#">Accueil</a></li>
             <li><a href="#">Évènements</a></li>
@@ -9,130 +8,16 @@
         <div class="section-title left" data-animate="fadeInUp">
             <h1>Calendrier</h1>
         </div>
+        <?php $mois_all = array(
+                "janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"
+        );?>
         <div class="row">
             <div class="col-md-9">
+                <?php foreach ($mois_all as $mois):?>
+                    <span class="month-anchor" id="mois-<?php echo $mois;?>"></span>
                 <div class="evenement-mois">
                     <div class="section-title left" data-animate="fadeInUp">
-                        <h3>Juin</h3>
-                    </div>
-                    <div class="liste-evenements">
-                        <div class="list-item">
-                            <div class="row">
-                                <a href="https://scontent-mrs1-1.xx.fbcdn.net/v/t1.0-9/31277589_1651529734954992_6658242169030714875_n.jpg?_nc_cat=0&oh=19dc7941e95d351f99d9c81380fe82aa&oe=5B95021D" class="popup-link">
-                                <div class="col-xs-5">
-
-                                        <img src="https://scontent-mrs1-1.xx.fbcdn.net/v/t1.0-9/31277589_1651529734954992_6658242169030714875_n.jpg?_nc_cat=0&oh=19dc7941e95d351f99d9c81380fe82aa&oe=5B95021D" class="img-responsive" alt="Photo evenement">
-
-
-                                </div>
-                                <div class="col-xs-7">
-                                    <div class="event_title">Adango en café concert</div>
-                                    <div class="event_location">L'Institut français du Gabon</div>
-                                    <div class="event_date">9 Juin 2018 à 19h30</div>
-                                    <div class="annonceur">Par L'Institut français du Gabon</div>
-                                </div>
-                            </div>
-                            </a>
-                        </div>
-
-                        <div class="list-item">
-                            <div class="row">
-                                <a href="https://scontent-mrs1-1.xx.fbcdn.net/v/t1.0-9/29186965_1610944632346836_3342431140360164448_n.jpg?_nc_cat=0&oh=9f8f46a7d12bd35b9e1fc1b7c7ad10d3&oe=5B9CD38D" class="popup-link">
-                                <div class="col-xs-5">
-                                        <img src="https://scontent-mrs1-1.xx.fbcdn.net/v/t1.0-9/29186965_1610944632346836_3342431140360164448_n.jpg?_nc_cat=0&oh=9f8f46a7d12bd35b9e1fc1b7c7ad10d3&oe=5B9CD38D" class="img-responsive" alt="Photo evenement">
-                                </div>
-                                <div class="col-xs-7">
-                                    <div class="event_title">Matris</div>
-                                    <div class="event_location">L'Institut français du Gabon</div>
-                                    <div class="event_date">9 Juin 2018 à 19h30</div>
-                                    <div class="annonceur">Par L'Institut français du Gabon</div>
-                                </div>
-                                </a>
-                            </div>
-                        </div>
-
-                        <div class="list-item">
-                            <div class="row">
-                                <a href="https://scontent-mrs1-1.xx.fbcdn.net/v/t1.0-9/30656512_1640560252718607_3635224169070276705_n.jpg?_nc_cat=0&oh=77374de791fbebb84b017074c2245163&oe=5B4FB868" class="popup-link">
-                                <div class="col-xs-5">
-                                        <img src="https://scontent-mrs1-1.xx.fbcdn.net/v/t1.0-9/30656512_1640560252718607_3635224169070276705_n.jpg?_nc_cat=0&oh=77374de791fbebb84b017074c2245163&oe=5B4FB868" class="img-responsive" alt="Photo evenement">
-                                </div>
-                                <div class="col-xs-7">
-                                    <div class="event_title">Expositios  photos : Passionnés / Passionnelles de Emmanuelle Laté</div>
-                                    <div class="event_location">L'Institut français du Gabon</div>
-                                    <div class="event_date">9 Juin 2018 à 19h30</div>
-                                    <div class="annonceur">Par l'Institut français du Gabon</div>
-                                </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <span id="mois-juillet"></span>
-                </div>
-
-                <div class="evenement-mois">
-                    <div class="section-title left" data-animate="fadeInUp">
-                        <h3>Juillet</h3>
-                    </div>
-                    <div class="liste-evenements">
-                        <div class="list-item">
-                            <div class="row">
-                                <a href="https://scontent-mrs1-1.xx.fbcdn.net/v/t1.0-9/31277589_1651529734954992_6658242169030714875_n.jpg?_nc_cat=0&oh=19dc7941e95d351f99d9c81380fe82aa&oe=5B95021D" class="popup-link">
-                                    <div class="col-xs-5">
-
-                                        <img src="https://scontent-mrs1-1.xx.fbcdn.net/v/t1.0-9/31277589_1651529734954992_6658242169030714875_n.jpg?_nc_cat=0&oh=19dc7941e95d351f99d9c81380fe82aa&oe=5B95021D" class="img-responsive" alt="Photo evenement">
-
-
-                                    </div>
-                                    <div class="col-xs-7">
-                                        <div class="event_title">Adango en café concert</div>
-                                        <div class="event_location">L'Institut français du Gabon</div>
-                                        <div class="event_date">9 Juin 2018 à 19h30</div>
-                                        <div class="annonceur">Par L'Institut français du Gabon</div>
-                                    </div>
-                            </div>
-                            </a>
-                        </div>
-
-                        <div class="list-item">
-                            <div class="row">
-                                <a href="https://scontent-mrs1-1.xx.fbcdn.net/v/t1.0-9/29186965_1610944632346836_3342431140360164448_n.jpg?_nc_cat=0&oh=9f8f46a7d12bd35b9e1fc1b7c7ad10d3&oe=5B9CD38D" class="popup-link">
-                                    <div class="col-xs-5">
-                                        <img src="https://scontent-mrs1-1.xx.fbcdn.net/v/t1.0-9/29186965_1610944632346836_3342431140360164448_n.jpg?_nc_cat=0&oh=9f8f46a7d12bd35b9e1fc1b7c7ad10d3&oe=5B9CD38D" class="img-responsive" alt="Photo evenement">
-                                    </div>
-                                    <div class="col-xs-7">
-                                        <div class="event_title">Matris</div>
-                                        <div class="event_location">L'Institut français du Gabon</div>
-                                        <div class="event_date">9 Juin 2018 à 19h30</div>
-                                        <div class="annonceur">Par L'Institut français du Gabon</div>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-
-                        <div class="list-item">
-                            <div class="row">
-                                <a href="https://scontent-mrs1-1.xx.fbcdn.net/v/t1.0-9/30656512_1640560252718607_3635224169070276705_n.jpg?_nc_cat=0&oh=77374de791fbebb84b017074c2245163&oe=5B4FB868" class="popup-link">
-                                    <div class="col-xs-5">
-                                        <img src="https://scontent-mrs1-1.xx.fbcdn.net/v/t1.0-9/30656512_1640560252718607_3635224169070276705_n.jpg?_nc_cat=0&oh=77374de791fbebb84b017074c2245163&oe=5B4FB868" class="img-responsive" alt="Photo evenement">
-                                    </div>
-                                    <div class="col-xs-7">
-                                        <div class="event_title">Expositios  photos : Passionnés / Passionnelles de Emmanuelle Laté</div>
-                                        <div class="event_location">L'Institut français du Gabon</div>
-                                        <div class="event_date">9 Juin 2018 à 19h30</div>
-                                        <div class="annonceur">Par l'Institut français du Gabon</div>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <span id="mois-aout"></span>
-                </div>
-
-                <div class="evenement-mois">
-                    <div class="section-title left" data-animate="fadeInUp">
-                        <h3>Août</h3>
+                        <h3><?php echo $mois;?></h3>
                     </div>
                     <div class="liste-evenements">
                         <div class="list-item">
@@ -187,11 +72,12 @@
                         </div>
                     </div>
                 </div>
+                <?php endforeach;?>
             </div>
             <?php include "assets/aside-evenements.php"; ?>
         </div>
         <ul id="menu-mois" class="fixed">
-            <li><a href="#mois-janvier">Jan</a></li>
+            <li><a href="#">Jan</a></li>
             <li><a href="#mois-fevrier">Fev</a></li>
             <li><a href="#mois-mars">Mar</a></li>
             <li><a href="#mois-avril">Avr</a></li>
