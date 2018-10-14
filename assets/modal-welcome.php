2fd640225eb123bc9fa2f6c4f70e0ec562660e3e
Alexandre Kaze <zekaze@users.noreply.github.com>
2018-10-14 15:29:30 +0100

    Updates: - Modale welcome

--- assets/modal-welcome.php
+++ assets/modal-welcome.php
@@ -1,6 +1,3 @@
-<!-- Trigger the modal with a button -->
-<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
-
 <!-- Modal -->
 <div id="myModal" class="modal fade" role="dialog">
     <div class="modal-dialog modal-lg">
@@ -8,6 +5,7 @@
         <!-- Modal content-->
         <div class="modal-content">
             <div class="modal-body">
+                <div class="cover-image"></div>
                 <h2>Bienvenue chez <br>Mommy & Me Gabon</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores, commodi corporis debitis delectus dicta est ex explicabo facere itaque iure officia provident, repellendus sequi temporibus totam, voluptas voluptates voluptatibus.</p>
             </div>
